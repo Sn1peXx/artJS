22f885b9110cc0de268dcc87336c4a5e126cb6b9
Andrey Lavrusenko <andreylavrusenko@MacBook-Air-Andrey.local>
2021-06-11 15:47:49 +0300

    add forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import modal from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
     modal();
     sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms()
 }); 
